60bc88c2dc85c1ad761561b311b4487af815469f
Mahmud <mahmudur69@student.sust.edu>
2023-05-21 20:50:35 +0600

    In detail description to run this project and documentation for the api req and res

--- backend/src/validators/auth.route.validator.js
+++ backend/src/validators/auth.route.validator.js
@@ -12,31 +12,61 @@ const { StatusCode, HttpError } = require('../utils/commonObject.util');
  */
 const signup = [
 
-    body('name').trim().not()
-        .isEmpty().withMessage('Name is required'),
+    body('name')
+        .trim()
+        .not()
+        .optional()
+        .notEmpty()
+        .withMessage('Name is required')
+        .isLength({ max: 30 })
+        .withMessage('Name must be at max 30 characters'),
 
-    body('email').trim().not()
-        .isEmpty().withMessage('Email is required')
+    body('email')
+        .trim()
+        .not()
+        .optional()
+        .isEmpty()
+        .withMessage('Email is required')
         .if(body('email').notEmpty())
-        .isEmail().withMessage('Invalid email address').normalizeEmail(),
+        .isEmail()
+        .withMessage('Invalid email address')
+        .normalizeEmail(),
 
-    body('username').trim().not()
-        .isEmpty().withMessage('Username is required')
+    body('username')
+        .trim()
+        .not()
+        .optional()
+        .notEmpty()
+        .withMessage('Username is required')
         .if(body('username').notEmpty())
-        .isLength({ min: 4 }).withMessage('Username must be at least 4 characters'),
+        .isLength({ min: 4 })
+        .withMessage('Username must be at least 4 characters')
+        .isLength({ max: 20 })
+        .withMessage('Username must be at max 20 characters'),
 
     body('password')
-        .trim().not()
-        .isEmpty().withMessage('Password is required')
+        .trim()
+        .not()
+        .optional()
+        .isEmpty()
+        .withMessage('Password is required')
         .if(body('password').notEmpty())
-        .isLength({ min: 4 }).withMessage('Password must be at least 4 characters'),
+        .isLength({ min: 4 })
+        .withMessage('Password must be at least 4 characters')
+        .isLength({ max: 20 })
+        .withMessage('Password must be at max 20 characters'),
 
-    body('confirmPassword').custom((value, { req }) => {
-        if (value !== req.body.password) {
-            throw new HttpError(StatusCode.BAD_REQUEST, 'Passwords do not match');
-        }
-        return true;
-    }).optional()
+    body('confirmPassword')
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new HttpError(
+                    StatusCode.BAD_REQUEST,
+                    'Passwords do not match'
+                );
+            }
+            return true;
+        })
+        .optional(),
 
 ];
 
@@ -48,13 +78,19 @@ const login = [
     body('username').trim().not()
         .isEmpty().withMessage('Username is required')
         .if(body('username').notEmpty())
-        .isLength({ min: 4 }).withMessage('Username must be at least 4 characters'),
+        .isLength({ min: 4 })
+        .withMessage('Username must be at least 4 characters')
+        .isLength({ max: 20 })
+        .withMessage('Username must be at max 20 characters'),
 
     body('password')
         .trim().not()
         .isEmpty().withMessage('Password is required')
         .if(body('password').notEmpty())
-        .isLength({ min: 4 }).withMessage('Password must be at least 4 characters')
+        .isLength({ min: 4 })
+        .withMessage('Password must be at least 4 characters')
+        .isLength({ max: 20 })
+        .withMessage('Password must be at max 20 characters')
 ];
 
 module.exports = { signup, login };

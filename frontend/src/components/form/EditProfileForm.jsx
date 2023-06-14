2ee202bd6111dbc1f2b6c95deea153fe83314764
MD <mahmudur.rahman@cefalo.com>
2023-06-14 13:56:59 +0600

    refactor singleblog

--- frontend/src/components/form/EditProfileForm.jsx
+++ frontend/src/components/form/EditProfileForm.jsx
@@ -5,9 +5,9 @@ import validateInputs from '../../utils/formValidation.util';
 export default function EditProfileForm({ onSubmit, profileData, error, setError }) {
 
     const [inputs, setInputs] = useState({
-        name: profileData?.name,
-        email: profileData?.email,
-        username: profileData?.username
+        name: profileData?.name
+        // email: profileData?.email,
+        // username: profileData?.username
     });
 
     const handleChange = (event) => {
@@ -47,7 +47,7 @@ export default function EditProfileForm({ onSubmit, profileData, error, setError
                         className={`w-full px-3 py-2 border rounded focus:outline-none ${error ? 'border-red-500' : 'border-gray-300 focus:border-blue-500'}`}
                     />
                 </div>
-                <div className="mb-4">
+                {/* <div className="mb-4">
                     <label htmlFor="email" className="block font-medium mb-1">
                         Email
                     </label>
@@ -76,14 +76,16 @@ export default function EditProfileForm({ onSubmit, profileData, error, setError
                         required pattern="[a-z A-Z 0-9]+"
                         className={`w-full px-3 py-2 border rounded focus:outline-none ${error ? 'border-red-500' : 'border-gray-300 focus:border-blue-500'}`}
                     />
-                </div>
+                </div> */}
                 <ErrorShow error={error} />
-                <button
-                    type="submit"
-                    className="w-full bg-blue-500 text-white rounded px-4 py-2 hover:bg-blue-600"
-                >
-                    Update Profile
-                </button>
+                <div className="flex justify-center">
+                    <button
+                        type="submit"
+                        className="w-full bg-blue-500 text-white rounded px-4 py-2 hover:bg-blue-600"
+                    >
+                        SAVE
+                    </button>
+                </div>
             </form>
         </>
     )
--- frontend/src/components/nav/profileDropDown.jsx
+++ frontend/src/components/nav/profileDropDown.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOutAlt, faCog, faUser, faGreaterThan, faLessThan } from '@fortawesome/free-solid-svg-icons';
 import { AuthContext } from '../../contexts/AuthContext';
@@ -12,11 +12,17 @@ import Logout from '../auth/Logout';
 
 function ProfileDropdown() {
     const navigate = useNavigate();
+    const dropdownRef = useRef(null);
+    const dropdownButtonRef = useRef(null);
     const { authData, setAuthData, setIsLoggedIn } = useContext(AuthContext);
     const [isOpen, setIsOpen] = useState(false);
 
     const handleToggle = () => {
         setIsOpen(!isOpen);
+    };
+
+    const closeDropdown = () => {
+        setIsOpen(false);
     };
 
     const handleOptionClick = (option) => {
@@ -31,10 +37,29 @@ function ProfileDropdown() {
         }
     };
 
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (
+                dropdownRef.current &&
+                !dropdownRef.current.contains(event.target) &&
+                !dropdownButtonRef.current.contains(event.target)
+            ) {
+                closeDropdown();
+            }
+        };
+
+        document.addEventListener('click', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, []);
+
     return (
         <div className="relative text-lg  lg:rounded text-white lg:text-blue-600 hover:text-blue-600 hover:bg-gray-100">
             <div
                 type="button"
+                ref={dropdownButtonRef}
                 className="w-full flex py-3 px-5 items-center focus:outline-none hover:cursor-pointer"
                 onClick={handleToggle}
             >
@@ -57,7 +82,7 @@ function ProfileDropdown() {
             </div>
 
             {isOpen && (
-                <ul className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1">
+                <ul ref={dropdownRef} className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1">
                     <li>
                         <button
                             className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left "
